dc69291ad57d48001948703413cf091df24d7056
Ahmed Waleed <wello.3245@gmail.com>
2023-02-13 22:14:27 +0200

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define endl "\n"
+using namespace std;
+
+const double PI = 3.141592653589793238462643383279502884197;
+const ll INF = 2e9;
+const ll MOD = 1e9 + 7;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ios_base::sync_with_stdio(false);   cin.tie(NULL);  cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int t = 1;
+    //cin>>t;
+    while (t--) {
+
+    }
+
     return 0;
 }
